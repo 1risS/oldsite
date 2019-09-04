9ebe956706dab10d49f960b4e61bd2c42e5620b7
Iris Saladino <iris.saladino@globallogic.com>
2019-09-04 16:05:24 -0300

    p5 adentro

--- src/components/sketch.js
+++ src/components/sketch.js
@@ -1,102 +1,108 @@
-
-
-export default function sketch (p) {
-    let sprites = [];
-    let r = 0;
-    let fontName = null;
-
-    function preload() {
-    // uncomment for custom font
-    // fontName = loadFont("assets/Verlag.ttf");
+export default function sketch(p) {
+    let sprites = []
+    let r = 0
+    //let fontName = null
+  
+    p.preload = () => {
+      // uncomment for custom font
+      // fontName = loadFont("assets/Verlag.ttf");
     }
-
-    function setup() {
-    createCanvas(600, 200);
-
-    sprites = createFontSprites("i r i s S", {
+  
+    p.setup = () => {
+      p.createCanvas(300, 100)
+  
+      sprites = createFontSprites('i r i s S', {
         // font: fontName,
         brightnessThreshold: 40,
         step: 5,
-        fontSize: 100
-    });
+        fontSize: 100,
+      })
     }
-
-    function draw() {
-    r += 0.025;
-
-    background(0);
-    stroke(201, 176, 255);  // font color
-    fill(255);
-
-    for (let i = 0; i < sprites.length; i++) {
-        const sprite = sprites[i];
-        sprite.draw(r);
-        const closeSprites = findCloseSprites(sprite);
+  
+    p.draw = () => {
+      r += 0.025
+  
+      p.background(32,24,21)
+      p.stroke(201, 176, 255) // font color
+      p.fill(255)
+  
+      for (let i = 0; i < sprites.length; i++) {
+        const sprite = sprites[i]
+        sprite.draw(r)
+        const closeSprites = findCloseSprites(sprite)
         for (let j = 0; j < closeSprites.length; j++) {
-        const closeSprite = closeSprites[j];
-        line(sprite.vector.x, sprite.vector.y, closeSprite.vector.x, closeSprite.vector.y);
+          const closeSprite = closeSprites[j]
+          p.line(
+            sprite.vector.x,
+            sprite.vector.y,
+            closeSprite.vector.x,
+            closeSprite.vector.y
+          )
         }
+      }
     }
-    }
-
+  
     function createFontSprites(string, opts) {
-    const font = opts.font || "Helvetica";
-    const brightnessThreshold = opts.brightnessThreshold || 40;
-    const step = opts.step || 5;
-    const fontSize = opts.fontSize || 120;
-    
-    const res = [];
-
-    const pg = createGraphics(width, height);
-
-    pg.fill(255);
-    pg.textFont(font, fontSize);
-    pg.text(string, 0, fontSize - 20);
-
-    for (let x = 0; x < pg.width; x += step) {
+      const font = opts.font || 'Helvetica'
+      const brightnessThreshold = opts.brightnessThreshold || 40
+      const step = opts.step || 5
+      const fontSize = opts.fontSize || 120
+  
+      const res = []
+  
+      const pg = p.createGraphics(p.width, p.height)
+  
+      pg.fill(255)
+      pg.textFont(font, fontSize)
+      pg.text(string, 0, fontSize - 20)
+  
+      for (let x = 0; x < pg.width; x += step) {
         for (let y = 0; y < pg.height; y += step) {
-        const c = color(pg.get(x, y));
-
-        if (brightness(c) > brightnessThreshold) {
-            res.push(new Sprite(createVector(x, y)));
+          const c = p.color(pg.get(x, y))
+  
+          if (p.brightness(c) > brightnessThreshold) {
+            res.push(new Sprite(p.createVector(x, y)))
+          }
         }
-        }
+      }
+      // console.log("Sprites:", res.length);
+  
+      return res
     }
-    // console.log("Sprites:", res.length);
-
-    return res;
-    }
-
+  
     function findCloseSprites(targetSprite) {
-    const closeSprites = [];
-
-    for (let i = 0; i < sprites.length; i++) {
-        const sprite = sprites[i];
+      const closeSprites = []
+  
+      for (let i = 0; i < sprites.length; i++) {
+        const sprite = sprites[i]
         if (sprite == targetSprite) {
-        break;
+          break
         }
-
-        if (sprite.vector.dist(targetSprite.vector) < (sin(frameCount / 200) * 10 + 10)) {
-        closeSprites.push(sprite);
+  
+        if (
+          sprite.vector.dist(targetSprite.vector) <
+          p.sin(p.frameCount / 200) * 10 + 10
+        ) {
+          closeSprites.push(sprite)
         }
+      }
+  
+      return closeSprites
     }
-
-    return closeSprites;
-    }
-
+  
     class Sprite {
-    constructor(vector) {
-        this.vector = null;
-        this.originalVector = vector;
-        this.difference = random(20);
+      constructor(vector) {
+        this.vector = null
+        this.originalVector = vector
+        this.difference = p.random(20)
+      }
+  
+      draw(r) {
+        const x = p.cos(this.difference + r) * 4 + this.originalVector.x
+        const y = p.sin(this.difference + r) * 4 + this.originalVector.y
+        this.vector = p.createVector(x, y)
+  
+        p.ellipse(x, y, 2, 2)
+      }
     }
-
-    draw(r) {
-        const x = cos(this.difference + r) * 4 + this.originalVector.x;
-        const y = sin(this.difference + r) * 4 + this.originalVector.y;
-        this.vector = createVector(x, y);
-
-        ellipse(x, y, 2, 2);
-    }
-    }
-}
+  }
